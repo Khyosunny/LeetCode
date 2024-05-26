4953dcba99261a130c98eb6c427d9640c91706aa
Hyosun Kim <kimhyo_0218@naver.com>
2024-05-26 19:32:44 +0900

    Time: 95 ms, Memory: 56 MB - LeetHub

--- squares-of-a-sorted-array/squares-of-a-sorted-array.js
+++ squares-of-a-sorted-array/squares-of-a-sorted-array.js
@@ -3,7 +3,18 @@
  * @return {number[]}
  */
 var sortedSquares = function(nums) {
-    return nums.map((num) => {
-        return num * num;
-    }).sort((a, b) => a - b);
+    let start = 0;
+    let end = nums.length - 1;
+    let array = new Array(nums.length);
+    
+    for (let i = nums.length - 1; i >= 0; i--) {
+        if (nums[start] * nums[start] > nums[end] * nums[end]) {
+            array[i] = nums[start] * nums[start];
+            start++;
+        } else {
+            array[i] = nums[end] * nums[end];
+            end--;
+        }
+    }
+    return array;
 };
