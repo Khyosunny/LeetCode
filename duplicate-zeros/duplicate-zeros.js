8d7f940728333923c298d61d43359f417caf7674
Hyosun Kim <kimhyo_0218@naver.com>
2024-05-28 22:01:32 +0900

    Time: 63 ms, Memory: 51.4 MB - LeetHub

---
+++ duplicate-zeros/duplicate-zeros.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} arr
+ * @return {void} Do not return anything, modify arr in-place instead.
+ */
+var duplicateZeros = function(arr) {
+    let length = arr.length;
+    
+    for (let i = 0; i < length - 1; i++) {
+        if (arr[i] === 0) {
+            for (let j = length - 2; j >= i; j--) {
+                arr[j + 1] = arr[j];
+            }
+            i++;
+        }
+        
+    }
+    
+};
