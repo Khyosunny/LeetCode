a446cefa4dde881f2dfc9d371a3ddc59b7de5da6
Hyosun Kim <kimhyo_0218@naver.com>
2024-05-29 21:56:05 +0900

    Time: 63 ms, Memory: 49.1 MB - LeetHub

--- merge-sorted-array/merge-sorted-array.js
+++ merge-sorted-array/merge-sorted-array.js
@@ -6,13 +6,28 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function(nums1, m, nums2, n) {
-    const length = m + n;
-    let p1 = 0;
-    for (let i = m; i < length; i++) {
-        if (n === 0) return;
-        if (nums1[i] === 0) {
-            nums1[i] = nums2[p1++];
+    // const length = m + n;
+    // let j = 0;
+    // for (let i = m; i < length; i++) {
+    //     if (n === 0) return;
+    //     if (nums1[i] === 0) {
+    //         nums1[i] = nums2[j++];
+    //     }
+    // }
+    // nums1.sort((a, b) => a - b);
+    
+    let i = m - 1;
+    let j = n - 1;
+    let k = nums1.length - 1;
+    
+    while(j >= 0) {
+        if (nums1[i] > nums2[j]) {
+            nums1[k] = nums1[i];
+            i--;
+        } else {
+            nums1[k] = nums2[j];
+            j--;
         }
+        k--;
     }
-    nums1.sort((a, b) => a - b);
 };
