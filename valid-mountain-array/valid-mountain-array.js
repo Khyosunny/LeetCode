d41fce9715a0a02a4173c590bbb36006c636d697
Hyosun Kim <kimhyo_0218@naver.com>
2024-06-30 21:22:54 +0900

    Time: 46 ms, Memory: 50.8 MB - LeetHub

--- valid-mountain-array/valid-mountain-array.js
+++ valid-mountain-array/valid-mountain-array.js
@@ -3,21 +3,41 @@
  * @return {boolean}
  */
 var validMountainArray = function(arr) {
-    const max = Math.max(...arr);
-    const maxIndex = arr.indexOf(max);
+//     const max = Math.max(...arr);
+//     const maxIndex = arr.indexOf(max);
+    
+//     if (arr.length < 3) return false;
+//     if (maxIndex === 0) return false;
+//     if (maxIndex === arr.length - 1) return false;
+    
+//     for (let i = 0; i < maxIndex; i++) {
+//         if (arr[i] >= arr[i + 1]) return false;
+//     }
+    
+//     for (let i = maxIndex; i < arr.length; i++) {
+//         if (arr[i] <= arr[i + 1]) return false;
+//     }
+    
+//     return true;
     
     if (arr.length < 3) return false;
-    if (maxIndex === 0) return false;
-    if (maxIndex === arr.length - 1) return false;
     
-    for (let i = 0; i < maxIndex; i++) {
-        console.log(arr[i] >= arr[i + 1]);
-        if (arr[i] >= arr[i + 1]) return false;
+    let left = 0;
+    let right = arr.length - 1;
+    
+    for (let i = 0; i < arr.length; i++) {
+        if (left > right) {
+            return false;
+        }
+        if (arr[left] < arr[left + 1]) {
+            left++;
+        }
+        if (arr[right] < arr[right - 1]) {
+            right--;
+        }
     }
     
-    for (let i = maxIndex; i < arr.length; i++) {
-        if (arr[i] <= arr[i + 1]) return false;
-    }
+    if (left === 0 || right === arr.length - 1) return false;
     
-    return true;
+    return left === right;
 };
